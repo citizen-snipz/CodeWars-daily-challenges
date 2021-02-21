1544d4d203df2d3ec2e6edf2baadc530daa3ae85
Shara Crosslin <sharacrosslin@gmail.com>
2021-02-21 13:07:32 -0800

    added 2-21-21 challenges

---
+++ 022121.js
@@ -0,0 +1,19 @@
+// Area or Perimeter
+// https://www.codewars.com/kata/5ab6538b379d20ad880000ab/solutions/javascript
+const areaOrPerimeter = (l, w) => (l === w ? l * w : 2 * (l + w));
+
+// Filling an array (part 1)
+// https://www.codewars.com/kata/571d42206414b103dc0006a1/solutions/javascript
+const arr = (N) => {
+  numArr = [];
+  for (let i = 0; i < N; i++) {
+    numArr.push(i);
+  }
+  return numArr;
+};
+
+//Grasshopper - If/else syntax debug
+//https://www.codewars.com/kata/57089707fe2d01529f00024a/train/javascript
+function checkAlive(health) {
+  return health > 0;
+}
