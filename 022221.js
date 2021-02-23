1552ec0f3bb45212d4893780c7186e647e3b29e3
Shara Crosslin <sharacrosslin@gmail.com>
2021-02-22 17:59:13 -0800

    more solutions for 2-22-21

--- 022221.js
+++ 022221.js
@@ -7,3 +7,28 @@ function seatsInTheater(nCols, nRows, col, row) {
 // Dollars and Cents
 // https://www.codewars.com/kata/55902c5eaa8069a5b4000083/train/javascript
 const formatMoney = (amount) => `$${amount.toFixed(2)}`;
+
+// Filter out the geese
+// https://www.codewars.com/kata/57ee4a67108d3fd9eb0000e7/train/javascript
+function gooseFilter(birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  return birds.filter((bird) => !geese.includes(bird));
+}
+
+//Sort and Star
+// https://www.codewars.com/kata/57cfdf34902f6ba3d300001e/train/javascript
+function twoSort(s) {
+  s.sort();
+  return s[0].split("").join("***");
+}
+
+//Hello, Name or World!
+// https://www.codewars.com/kata/57e3f79c9cb119374600046b/train/javascript
+function hello(name) {
+  if (name === "" || !name) {
+    return "Hello, World!";
+  } else {
+    let formattedName = name[0].toUpperCase() + name.slice(1).toLowerCase();
+    return `Hello, ${formattedName}!`;
+  }
+}
