30bef52e78ae661dd711d58daf641acf1b6d01a6
Shara Crosslin <sharacrosslin@gmail.com>
2021-03-05 18:47:45 -0800

    added solution 3-5-21

--- 030521.js
+++ 030521.js
@@ -4,3 +4,13 @@
 function reverse(string) {
   return string.split(" ").reverse().join(" ");
 }
+
+// Twice as old
+// https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return Math.abs(sonYearsOld * 2 - dadYearsOld);
+}
+
+// The falling speed of petals
+// https://www.codewars.com/kata/5a0be7ea8ba914fc9c00006b/train/javascript
+const sakuraFall = (v) => (v <= 0 ? 0 : 400 / v);
