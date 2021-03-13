76e136599a4b52c11cc4b50df73caff7df00990b
Shara Crosslin <sharacrosslin@gmail.com>
2021-03-13 14:34:37 -0800

    added solutions for 3-13-21

--- 031221.js
+++ 031221.js
@@ -13,3 +13,13 @@ function powersOfTwo(n) {
 function mouthSize(animal) {
   return animal.toLowerCase() === "alligator" ? "small" : "wide";
 }
+
+// Pre-FizzBuzz Workout #1
+// https://www.codewars.com/kata/569e09850a8e371ab200000b/train/javascript
+function preFizz(n) {
+  let arr = [];
+  for (let i = 1; i <= n; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
