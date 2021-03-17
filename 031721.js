e01cea035771d01c37b9a92c3b75a3986c8fadbd
Shara Crosslin <sharacrosslin@gmail.com>
2021-03-17 15:24:56 -0700

    added solutions for 3-17-21

---
+++ 031721.js
@@ -0,0 +1,19 @@
+// Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right
+// https://www.codewars.com/kata/57faf7275c991027af000679/train/javascript
+function remove(s, n) {
+  let markCount = "";
+  let str = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == "!" && markCount.length < n) {
+      markCount += s[i];
+    } else {
+      str += s[i];
+    }
+  }
+  return str;
+}
+
+// Take the Derivative
+// https://www.codewars.com/kata/5963c18ecb97be020b0000a2/train/javascript
+const derive = (coefficient, exponent) =>
+  `${coefficient * exponent}x^${exponent - 1}`;
