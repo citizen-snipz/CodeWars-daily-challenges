dc3430d54ab4663a006282426e2617b97d924451
Shara Crosslin <sharacrosslin@gmail.com>
2021-03-22 20:51:58 -0700

    added solutions for 3-22-21

---
+++ 032221.js
@@ -0,0 +1,7 @@
+// For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre
+// https://www.codewars.com/kata/582dafb611d576b745000b74/train/javascript
+const quote = function (fighter) {
+  return fighter.toLowerCase() === "george saint pierre"
+    ? "I am not impressed by your performance."
+    : "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+};
