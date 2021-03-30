85e4c4bf25e7adbd86ff7b505b48bd99c0a9210a
Shara Crosslin <sharacrosslin@gmail.com>
2021-03-29 20:37:00 -0700

    added solutions for 3-29-21

--- week-of-032821.js
+++ week-of-032821.js
@@ -7,3 +7,24 @@ function leo(oscar) {
   if (oscar < 88) return "When will you give Leo an Oscar?";
   else return "Leo got one already!";
 }
+
+//Mon 3-29-21
+// Multiple of index
+// https://www.codewars.com/kata/5a34b80155519e1a00000009/train/javascript
+function multipleOfIndex(array) {
+  return array.filter((num, idx) => num % idx === 0);
+}
+
+// "this" is a problem
+// https://www.codewars.com/kata/547c71fdc5b2b38db1000098/train/javascript
+function NameMe(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+  this.name = this.firstName + " " + this.lastName;
+}
+
+// String Templates - Bug Fixing #5
+// https://www.codewars.com/kata/55c90cad4b0fe31a7200001f/train/javascript
+function buildString(...template) {
+  return `I like ${template.join(", ")}!`;
+}
