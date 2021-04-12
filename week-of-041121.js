4761b198be88bb7ed9299d88c5e2741236718110
Shara Crosslin <sharacrosslin@gmail.com>
2021-04-12 16:46:25 -0700

    added solutions for 4-12-21

--- week-of-041121.js
+++ week-of-041121.js
@@ -9,3 +9,20 @@ function slope(points) {
 // Man in the west
 // https://www.codewars.com/kata/59bd5dc270a3b7350c00008b/train/javascript
 const checkTheBucket = (bucket) => bucket.includes("gold");
+
+// Monday 4/12/21
+// Training JS #10: loop statement --for
+// https://www.codewars.com/kata/5721a78c283129e416000999/train/javascript
+function pickIt(arr) {
+  var odd = [],
+    even = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      even.push(arr[i]);
+    } else {
+      odd.push(arr[i]);
+    }
+  }
+
+  return [odd, even];
+}
