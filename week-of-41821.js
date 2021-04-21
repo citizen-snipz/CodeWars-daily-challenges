1bb32538763c47e011b0f6751399524ab91233be
Shara Crosslin <sharacrosslin@gmail.com>
2021-04-21 16:59:09 -0700

    solutions for 4-22-21

--- week-of-41821.js
+++ week-of-41821.js
@@ -66,3 +66,11 @@ function evalObject(value) {
 }
 
 // UEFA EURO 2016
+// https://www.codewars.com/kata/57613fb1033d766171000d60/train/javascript
+function uefaEuro2016(teams, scores) {
+  return scores[0] > scores[1]
+    ? `At match ${teams[0]} - ${teams[1]}, ${teams[0]} won!`
+    : scores[0] < scores[1]
+    ? `At match ${teams[0]} - ${teams[1]}, ${teams[1]} won!`
+    : `At match ${teams[0]} - ${teams[1]}, teams played draw.`;
+}
