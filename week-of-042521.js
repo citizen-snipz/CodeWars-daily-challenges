62a81f7a59983fba42b9923c5fb6fb30603f37d4
Shara Crosslin <sharacrosslin@gmail.com>
2021-04-25 16:14:43 -0700

    solutions for 4-25-21

---
+++ week-of-042521.js
@@ -0,0 +1,6 @@
+// Sunday 4/25/21
+// Create Phone Number
+// https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/javascript
+function createPhoneNumber(n) {
+  return `(${n[0]}${n[1]}${n[2]}) ${n[3]}${n[4]}${n[5]}-${n[6]}${n[7]}${n[8]}${n[9]}`;
+}
