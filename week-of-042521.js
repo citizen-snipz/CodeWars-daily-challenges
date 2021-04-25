742c75b53da8a65ea369dcaa0ff67618f5311c34
Shara Crosslin <sharacrosslin@gmail.com>
2021-04-25 16:31:37 -0700

    4-25-21

--- week-of-042521.js
+++ week-of-042521.js
@@ -4,3 +4,18 @@
 function createPhoneNumber(n) {
   return `(${n[0]}${n[1]}${n[2]}) ${n[3]}${n[4]}${n[5]}-${n[6]}${n[7]}${n[8]}${n[9]}`;
 }
+
+// Complementary DNA
+// https://www.codewars.com/kata/554e4a2f232cdd87d9000038/train/javascript
+function DNAStrand(dna) {
+  let swap = {
+    A: "T",
+    T: "A",
+    C: "G",
+    G: "C"
+  };
+  return `${dna
+    .split("")
+    .map((letter) => swap[letter])
+    .join("")}`;
+}
