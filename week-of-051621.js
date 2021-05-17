3b5377e5232589f7085dc1c100477345ea2948cd
Shara Crosslin <sharacrosslin@gmail.com>
2021-05-16 19:36:25 -0700

    solutions for 5-16-21

---
+++ week-of-051621.js
@@ -0,0 +1,6 @@
+// Sunday 5/16/21
+// Friend or Foe?
+// https://www.codewars.com/kata/55b42574ff091733d900002f/train/javascript
+function friend(friends) {
+  return friends.filter((friend) => friend.length === 4);
+}
