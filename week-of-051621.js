87bc000c668f1f0726503c4f7505638d885b50c4
Shara Crosslin <sharacrosslin@gmail.com>
2021-05-20 21:09:59 -0700

    solutions for 5-20-21

--- week-of-051621.js
+++ week-of-051621.js
@@ -4,3 +4,14 @@
 function friend(friends) {
   return friends.filter((friend) => friend.length === 4);
 }
+
+// Thursday 5/20/21
+// Anagram Detection
+// https://www.codewars.com/kata/529eef7a9194e0cbc1000255/train/javascript
+// write the function isAnagram
+const isAnagram = function (test, original) {
+  return (
+    test.toLowerCase().split("").sort().join("") ===
+    original.toLowerCase().split("").sort().join("")
+  );
+};
