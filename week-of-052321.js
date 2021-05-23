64f3976f56de5a5a606db730be7c0b38d88b267f
Shara Crosslin <sharacrosslin@gmail.com>
2021-05-23 12:59:38 -0700

    solutions for 5-23-21

---
+++ week-of-052321.js
@@ -0,0 +1,30 @@
+// Sunday 5/23/21
+// Simple Fun #74: Growing Plant (7kyu)
+// https://www.codewars.com/kata/58941fec8afa3618c9000184/train/javascript
+function growingPlant(upSpeed, downSpeed, desiredHeight) {
+  let days = 1;
+  let plantHeight = upSpeed;
+
+  while (plantHeight < desiredHeight) {
+    plantHeight += upSpeed - downSpeed;
+    days++;
+  }
+  return days;
+}
+
+// Word values (7kyu)
+// https://www.codewars.com/kata/598d91785d4ce3ec4f000018/train/javascript
+function wordValue(a) {
+  const arr = [];
+
+  for (let i = 0; i < a.length; i++) {
+    let wordSum = 0;
+    for (let j = 0; j < a[i].length; j++) {
+      if (a[i].charCodeAt(j) - 96 >= 1) {
+        wordSum += a[i].charCodeAt(j) - 96;
+      }
+    }
+    arr.push(wordSum * (i + 1));
+  }
+  return arr;
+}
