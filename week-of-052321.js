8d365f3fd534a72599e4fa37ff8f2af25cdc1c70
Shara Crosslin <sharacrosslin@gmail.com>
2021-05-29 12:42:57 -0700

    solutions for 5-29-21

--- week-of-052321.js
+++ week-of-052321.js
@@ -33,3 +33,24 @@ function wordValue(a) {
 // Chuck Norris VII - True or False? (Beginner)
 // https://www.codewars.com/kata/570669d8cb7293a2d1001473/train/javascript
 const ifChuckSaysSo = () => !true
+
+const beginning = "               My cat loves my keyboard!"
+const both = "       First tryyyy!         "
+const end = "Loading...                           "
+
+beginning.trim() // My cat loves my keyboard!
+both.trim() // First tryyyy!
+end.trim() // Loading...
+
+// Saturday 5/29/21
+// Alternate capitalization
+// https://www.codewars.com/kata/59cfc000aeb2844d16000075/train/javascript
+function capitalize(s) {
+  const index1 = s
+    .split("")
+    .map((el, idx) => (idx % 2 === 0 ? el.toUpperCase() : el.toLowerCase()))
+  const index2 = s
+    .split("")
+    .map((el, idx) => (idx % 2 === 1 ? el.toUpperCase() : el.toLowerCase()))
+  return [index1.join(""), index2.join("")]
+}
