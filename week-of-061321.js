d2e832ba3595febe61e34433765efb69e0d928f3
Shara Crosslin <sharacrosslin@gmail.com>
2021-06-14 23:33:36 -0700

    solutions for 6-14-21

---
+++ week-of-061321.js
@@ -0,0 +1,14 @@
+// Monday 6/14/21
+// Sum of a sequence
+// https://www.codewars.com/kata/586f6741c66d18c22800010a/train/javascript
+const sequenceSum = (begin, end, step) => {
+  let count = 0
+  if (begin > end) {
+    return 0
+  } else {
+    for (let i = begin; i <= end; i += step) {
+      count += i
+    }
+    return count
+  }
+}
