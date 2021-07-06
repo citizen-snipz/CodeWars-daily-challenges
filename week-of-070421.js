675f43abb0210d2d77ffd593214691084a9de384
Shara Crosslin <sharacrosslin@gmail.com>
2021-07-06 15:10:52 -0700

    solutions for 7-6-21

--- week-of-070421.js
+++ week-of-070421.js
@@ -11,9 +11,17 @@ String.prototype.camelCase = function () {
 }
 
 // Tuesday 7/6/21
-// Remove duplicate words
+// Remove duplicate words (7kyu)
 //https://www.codewars.com/kata/5b39e3772ae7545f650000fc/train/javascript
 function removeDuplicateWords(s) {
   let sArr = s.split(" ")
   return [...new Set(sArr)].join(" ")
+}
+
+//CSV representation of array
+// https://www.codewars.com/kata/5a34af40e1ce0eb1f5000036/train/javascript
+function toCsvText(array) {
+  //let text = array.map((line, i) => i === array.length - 1 ? line : line + '\n')
+  const text = array.join("\n")
+  return `${text}`
 }
