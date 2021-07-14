36f69ae82a793a03f7de1ad54b09ec37bdf82707
Shara Crosslin <sharacrosslin@gmail.com>
2021-07-13 20:08:02 -0700

    solutions for 7-13-21

--- dailyStandup.js
+++ dailyStandup.js
@@ -236,3 +236,34 @@ console.log(checkForImposterSyndrome("I'm ready to interview!"))
 console.log(checkForImposterSyndrome("I can't interview yet!"))
 console.log(checkForImposterSyndrome("The pizza is ready"))
 console.log(checkForImposterSyndrome("The pizza is NOT ready"))
+
+// Create a CamelCase Method where all words must have their first letter capitalized without spaces. // For instance: // "hello case" => "HelloCase" // "camel case word" => "CamelCaseWord"
+
+// Always Truthy.
+// Always lower case change first to upper
+// Always have space
+
+// Same string exept withe letter upcase with no spaces
+// removeing spaces
+
+// "hello case" => "HelloCase"
+// "camel case word" => "CamelCaseWord"
+
+// take in set of words
+const camelCase = (str) => {
+  return str
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join("")
+}
+
+// we grab each word and change the first letter into uppercase
+// we are going to join each word back into a string without any spaces.
+// console.log(camelCase("hello case"))
+console.log(camelCase("hello case"), "HelloCase")
+console.log(camelCase("camel case word"), "CamelCaseWord")
+
+// this does work:
+
+// const camelCase = str =>
+//   str.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join('')
